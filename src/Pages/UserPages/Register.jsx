aaf82bd30886bdff05f584464f5f61223025c9c5
Adithyan <k85706035@gmail.com>
2025-08-18 15:57:46 +0530

    Register page added

--- src/Pages/UserPages/Register.jsx
+++ src/Pages/UserPages/Register.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 function Register() {
+    const [RegForm , setForm ] = useState({
+        name:'',
+        email:'',
+        password:'',
+    })
+
+    const HandleForm = (e) => {
+        setForm({...RegForm ,[e.target.name]: e.target.value})
+    }
+    console.log(RegForm);
+    
+    const HandleSubmit = async (e) => {
+        e.prevetDefault()
+
+        try{
+            const RegData = await axios.post('https://localhost:7023/api/Register',RegForm)
+        }catch(err){
+            console.error("Something fishy" , err)
+        }
+    }
+    
   return (
     <div>
-      Register
+      <form onSubmit={HandleSubmit}>
+        <label>Name</label>
+        <input type="text" value={RegForm.name} id='name' name='name' onChange={HandleForm}/>
+        <label>Email</label>
+        <input type="email" value={RegForm.email} id='email' name='email' onChange={HandleForm}/>
+        <label>Password</label>
+        <input type="password" value={RegForm.password} id='password' name='password' onChange={HandleForm}/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
